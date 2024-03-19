241b76d842214f65e86a02f9939500855f7c5b76
Vergburger <nicholas.johnson12c@gmail.com>
2024-03-18 22:16:19 -0500

    Fixed XML Layouts
    Fixed Settings and Scan XML layouts. Now should not have alignment issues. Also had list view example showing.

--- Android App/Alien PROBE/app/src/main/java/com/example/alienprobe/ScannerActivity.kt
+++ Android App/Alien PROBE/app/src/main/java/com/example/alienprobe/ScannerActivity.kt
@@ -3,6 +3,8 @@ package com.example.alienprobe
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 //import RFIDTag class
@@ -16,6 +18,16 @@ class ScannerActivity : AppCompatActivity() {
         buttonClick.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        // Get reference to the LinearLayout inside ScrollView
+        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout)
+
+        // Add 5 text items to the LinearLayout
+        for (i in 1..25) {
+            val textView = TextView(this)
+            textView.text = "Item $i"
+            linearLayout.addView(textView)
         }
 
         /*
