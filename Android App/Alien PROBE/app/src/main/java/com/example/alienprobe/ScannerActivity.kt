f9ee0431b591f9895ce286cc08f1d956f1d359fa
Vergburger <nicholas.johnson12c@gmail.com>
2024-03-24 19:33:25 -0500

    Changed ScannerActivity for tagList
    HOPEFULL Y WORK !

--- Android App/Alien PROBE/app/src/main/java/com/example/alienprobe/ScannerActivity.kt
+++ Android App/Alien PROBE/app/src/main/java/com/example/alienprobe/ScannerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
 //import RFIDTag class
@@ -30,20 +31,33 @@ class ScannerActivity : AppCompatActivity() {
             linearLayout.addView(textView)
         }
 
-        /*
         //start scanning for tags
-        val buttonClick = findViewById<Button>(R.id.)
+        val isScanner = findViewById<ToggleButton>(R.id.toggleScanner)
         buttonClick.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
 
-            //???
-            val tagGetter = AlienGetTagList();
+            //make AlienGetTagList() object
+            val tagGetter = AlienGetTagList()
+
+            // Check whether toggle is checked or not
+            if (isScanner.isChecked) {
+                //open reader
+                tagGetter.openReader();
+
+                // Get the tag list (Is static so get from class not the instance/object)
+                val tagList = AlienGetTagList.GetTagList()
+
+                // add value to scrollView
+                tagList?.forEach { tag ->
+                    val textView = TextView(this)
+                    textView.text = tag
+                    linearLayout.addView(textView)
+                }
+            } else {
+                // close the reader
+                tagGetter.closeReader();
+            }
+
         }
-        */
-
-        //stop scanning for tags
-
 
     }
 }
